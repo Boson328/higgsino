bbe08db8f0df5fd1818805dd344664aa28509091
Boson328 <121548464+Boson328@users.noreply.github.com>
2023-05-03 17:08:09 +0900

    入力動作ができるように

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import { kanaToRoman } from "./function/kanaToRoman";
 
+type result = {
+  isMiss: boolean;
+  isFinish: boolean;
+};
+
 export class Word {
   private _kanji: string = "";
   private _kana: string = "";
   private _roman: string[][] = [[""]];
   private _pattern: number[] = [0];
+  private _charIndex: number = 0;
+  private _charIndex2: number = 0;
+  private _charTyped: string = "";
+  private _isFinish: boolean = false;
 
   constructor(kanji: string, kana: string) {
     this._kanji = kanji;
@@ -21,15 +30,74 @@ export class Word {
     return this._kana;
   }
 
-  get roman() {
+  get roman(): {
+    text: string;
+    array: string[][];
+    typed: string[];
+    untyped: string[];
+  } {
     return {
-      text: this._roman.map((rome, index) => rome[this._pattern[index]]).join(""),
+      text: this._roman
+        .map((rome, index) => rome[this._pattern[index]])
+        .join(""),
       array: this._roman,
+      typed: this._roman.flatMap((rome, index) => {
+        if (index < this._charIndex) {
+          return rome[this._pattern[index]];
+        } else if (
+          index == this._charIndex &&
+          rome[this._pattern[index]].length > this._charIndex2
+        ) {
+          return rome[this._pattern[index]].slice(0, this._charIndex2);
+        } else return [];
+      }),
+      untyped: this._roman.flatMap((rome, index) => {
+        if (index > this._charIndex) {
+          return rome[this._pattern[index]];
+        } else if (index == this._charIndex) {
+          return rome[this._pattern[index]].slice(this._charIndex2);
+        } else return [];
+      }),
     };
   }
-}
 
-
-export function Hello() {
-  return "Hello World!"
+  typed(key: string): result {
+    if (!this._isFinish) {
+      // 文字を一つ一つ検査
+      if (this._roman[this._charIndex].includes("n")) {
+        for (let j = 0; j < this._roman[this._charIndex + 1].length; j++) {
+          if (this._roman[this._charIndex + 1][j][0] == key) {
+            this._pattern[this._charIndex] = 2;
+            this._pattern[this._charIndex + 1] = j;
+            this._charIndex++;
+            this._charIndex2 = 1;
+            this._charTyped = key;
+            return { isMiss: false, isFinish: false };
+          }
+        }
+      }
+      for (let i = 0; i < this._roman[this._charIndex].length; i++) {
+        const char = this._roman[this._charIndex][i].slice(
+          0,
+          this._charIndex2 + 1
+        );
+        if (char == this._charTyped + key) {
+          this._pattern[this._charIndex] = i;
+          this._charIndex2++;
+          this._charTyped += key;
+          if (this._charIndex2 >= this._roman[this._charIndex][i].length) {
+            this._charIndex++;
+            this._charIndex2 = 0;
+            this._charTyped = "";
+            if (this._roman.length <= this._charIndex) {
+              this._isFinish = true;
+              return { isMiss: false, isFinish: true };
+            }
+          }
+          return { isMiss: false, isFinish: false };
+        }
+      }
+      return { isMiss: true, isFinish: false };
+    } else return { isMiss: false, isFinish: true };
+  }
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,15 +1,15 @@
-import { Hello, Word } from "../src";
+import { Word } from "../src";
 import { kanaToRoman } from "../src/function/kanaToRoman";
 
 test("should first", () => {
-  expect(Hello()).toBe("Hello World!");
-  expect(kanaToRoman("ふぁっんあっ。")).toEqual([
-    ["fa", "hula", "huxa", "fula", "fuxa"],
-    ["ltu", "xtu", "ltsu", "xtsu"],
-    ["nn", "xn"],
-    ["a"],
-    ["ltu", "xtu", "ltsu", "xtsu"],
-    ["."],
-  ]);
-  console.log(new Word("漢字", "What is this?"))
+  const word = new Word("漢字", "かんな");
+  console.log(
+    word.typed("c"),
+    word.typed("a"),
+    word.typed("n"),
+    word.typed("n"),
+    word.typed("n"),
+    word.typed("a"),
+    word.roman
+  );
 });
